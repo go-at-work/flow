7f2cd60aa99fce479f3465df05c94bb4b4fc55f5
arisromil <arisromil@yahoo.com>
2025-05-12 11:18:19 -0600

    Update main.go

--- cmd/graphqlserver/main.go
+++ cmd/graphqlserver/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/arisromil/flow/config"
+)
 
 func main() {
-	fmt.Println("Hello, GraphQL server!")
+	ctx := context.Background()
+
+	conf := config.New()
+
+	db := porstgres.NewDB(ctx, conf)
+
+	if err := db.Migrate(); err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+
+	fmt.Println("migrated database")
+
 }
