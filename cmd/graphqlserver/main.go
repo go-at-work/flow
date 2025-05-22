f17a59b8ffec70f49b366d8e03645caa4291170e
arisromil <arisromil@yahoo.com>
2025-05-22 09:04:03 -0600

    Update main.go

--- cmd/graphqlserver/main.go
+++ cmd/graphqlserver/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"net/http"
+	"time"
 
+	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/arisromil/flow/config"
+	"github.com/arisromil/flow/domain"
+	"github.com/arisromil/flow/graph"
+	"github.com/arisromil/flow/postgres"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"golang.org/x/tools/playground"
 )
 
 func main() {
@@ -13,12 +21,29 @@ func main() {
 
 	conf := config.New()
 
-	db := porstgres.NewDB(ctx, conf)
+	db := postgres.NewDB(ctx, conf)
 
 	if err := db.Migrate(); err != nil {
 		log.Fatalf("failed to migrate database: %v", err)
 	}
 
-	fmt.Println("migrated database")
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.RedirectSlashes)
+	router.Use(middleware.Timeout(time.Second * 60))
+
+	userRepo := postgres.NewUserRepository(db)
+	authService := domain.NewAuthService(userRepo)
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", handler.NewDefaultServer(graph.NewExecutableSchema(
+		graph.Config{Resolvers: &graph.Resolver{
+			AuthService: authService,
+		}},
+	)))
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
