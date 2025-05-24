5d5d9f78fea2f50def8004c44091ada97af8d83c
arisromil <arisromil@yahoo.com>
2025-05-24 11:51:17 -0600

    Update user_resolver.go

--- graph/user_resolver.go
+++ graph/user_resolver.go
@@ -1,6 +1,19 @@
 package graph
 
-import "context"
+import (
+	"context"
+
+	"github.com/arisromil/flow"
+)
+
+func mapUser(user flow.User) *User {
+	return &User{
+		ID:        user.ID,
+		Username:  user.Username,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+	}
+}
 
 func (r *queryResolver) Me(ctx context.Context) (*User, error) {
 	panic("not implemented")
