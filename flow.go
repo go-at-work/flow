706ed55f6ec1460628ce23b7af032b95f89ce83f
arisromil <arisromil@yahoo.com>
2025-05-27 10:04:03 -0600

    Update flow.go

--- flow.go
+++ flow.go
@@ -3,7 +3,8 @@ package flow
 import "errors"
 
 var (
-	ErrBadCredentials  = errors.New("bad credentials")
-	ErrNotFound        = errors.New("user not found")
-	NewValidationError = errors.New("validation error")
+	ErrBadCredentials     = errors.New("bad credentials")
+	ErrNotFound           = errors.New("user not found")
+	NewValidationError    = errors.New("validation error")
+	ErrInvalidAccessToken = errors.New("invalid access token")
 )
