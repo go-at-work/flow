0eea0ec8b448b4cac782d9bf7b9f39cf8aa02463
arisromil <arisromil@yahoo.com>
2025-05-28 09:24:31 -0600

    Create resolver.go

---
+++ graph/resolver.go
@@ -0,0 +1,43 @@
+package graph
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/arisromil/flow"
+	"github.com/vektah/gqlparser/v2/gqlerror"
+)
+
+//go:generate go run github.com/99designs/gqlgen generate
+
+type Resolver struct {
+	AuthService flow.AuthService
+}
+
+type queryResolver struct {
+	*Resolver
+}
+
+func (r *queryResolver) Query() QueryResolver {
+	return &queryResolver{r}
+}
+
+type mutationResolver struct {
+	*Resolver
+}
+
+func (r *Resolver) Mutation() MutationResolver {
+	return &mutationResolver{r}
+
+}
+
+func buildBadRequestError(ctx context.Context, err error) error {
+	return &gqlerror.Error{
+		Message: err.Error(),
+		Path:    graphql.GetPath(ctx),
+		Extensions: map[string]interface{}{
+			"code": http.StatusBadRequest,
+		},
+	}
+}
