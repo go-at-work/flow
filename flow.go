809a5d0daac1b33fd7bb6e117eb34ef5b1bd969e
arisromil <arisromil@yahoo.com>
2025-06-04 09:16:58 -0600

    Update flow.go

--- flow.go
+++ flow.go
@@ -7,4 +7,5 @@ var (
 	ErrNotFound           = errors.New("user not found")
 	NewValidationError    = errors.New("validation error")
 	ErrInvalidAccessToken = errors.New("invalid access token")
+	ErrNoUserIdInContext  = errors.New("no user id in context")
 )
