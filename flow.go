9550eb70031084c80ff0593c92f377c2f75f90ba
arisromil <arisromil@yahoo.com>
2025-06-06 16:56:06 -0600

    Update flow.go

--- flow.go
+++ flow.go
@@ -8,4 +8,6 @@ var (
 	NewValidationError    = errors.New("validation error")
 	ErrInvalidAccessToken = errors.New("invalid access token")
 	ErrNoUserIdInContext  = errors.New("no user id in context")
+	ErrGenAccessToken     = errors.New("failed to generate access token")
+	ErrUnAuthenticated    = errors.New("unauthenticated")
 )
