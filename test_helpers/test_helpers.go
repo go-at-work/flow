fa26c42f3634b3cd66bf487d3911f5f132bcd0b5
arisromil <arisromil@yahoo.com>
2025-06-16 08:36:12 -0600

    Update test_helpers.go

--- test_helpers/test_helpers.go
+++ test_helpers/test_helpers.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/arisromil/flow"
+	"github.com/arisromil/flow/faker"
 	"github.com/arisromil/flow/postgres"
 	"github.com/stretchr/testify/require"
 )
@@ -14,4 +16,26 @@ func TeardownDB(ctx context.Context, t *testing.T, db *postgres.DB) {
 	err := db.Truncate(ctx)
 	require.NoError(t, err, "failed to truncate database")
 
+}
+
+func CreateUser(ctx context.Context, t *testing.T, userRepo flow.UserRepository) flow.User {
+	t.Helper()
+
+	userID, err := userRepo.Create(ctx, flow.User{
+		Username: faker.Username(),
+		Password: faker.Password,
+		Email:    faker.Email(),
+	})
+	require.NoError(t, err, "failed to create user")
+
+	return userID
+}
+
+func LoginUser(ctx context.Context, t *testing.T, userID flow.User) context.Context {
+	t.Helper()
+
+	token, err := userRepo.Login(ctx, userID)
+	require.NoError(t, err, "failed to login user")
+
+	return flow.PutUserIdToContext(ctx, token.Sub)
 }
